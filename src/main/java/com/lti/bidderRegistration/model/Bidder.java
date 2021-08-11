bb6a643865963bc4b86c8c6283a966ee3e6013c2
Prashu106 <prashusaxena23@gmail.com>
2021-08-11 11:05:17 +0530

    third Commit

--- src/main/java/com/lti/bidderRegistration/model/Bidder.java
+++ src/main/java/com/lti/bidderRegistration/model/Bidder.java
@@ -5,7 +5,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="BiderTable")
+@Table(name="BIDDER_TABLE")
 public class Bidder {
 	@Id
 	private String farmer_id;
